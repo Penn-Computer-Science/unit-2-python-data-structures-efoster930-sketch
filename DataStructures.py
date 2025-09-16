c91e95f75ea43aa36f9d5bf2c492417050626ed3
efoster930-sketch <efoster930@phm.k12.in.us>
2025-09-16 09:53:50 -0400

    Finished
    Added comments. done

--- DataStructures.py
+++ DataStructures.py
@@ -16,3 +16,31 @@ for item in numbers:
 #Part 2: Dictionaries
 countries = {"Colombia":"Bogota","Canada":"Toronto","Chile":"Santiago","South Africa":"Capetown","Italy":"Rome"}
 print(countries["Colombia"],countries["Canada"])
+countries["Germany"] = "Berlin"
+countries["Argentina"] = "Buenos Aires"
+print(countries)
+countries["Italy"] = "Florence"
+print(countries["Italy"])
+for item, value in countries.items():
+    print(f"Country: {item}, Capital: {value}")
+
+#Part 3: Sets
+fruit_set = {"Banana","Apple","Mango","Oranges","Watermelon"}
+print(fruit_set)
+fruit_set.add("Green Apple")
+print(fruit_set)
+fruit_set.remove("Mango")
+print(fruit_set)
+friend_fruits = {"Pineapple","Apple","Grapes","Banana"}
+intersection_set = fruit_set.intersection(friend_fruits)
+print(intersection_set)
+difference_set = fruit_set.difference(friend_fruits)
+print(difference_set)
+
+#Part 4: Colors
+tuples_colors = ("Red","Blue","Green","Yellow","Purple")
+print(tuples_colors[0],tuples_colors[4])
+for color in tuples_colors:
+    print(color)
+tuple[0] = "Black"
+#Raises a TypeError
--- gradebookManager.py
+++ gradebookManager.py
@@ -12,6 +12,7 @@ while set == True:
     if student_add == "Y":
         student_name = input("Name of the student? ")
         student_grades = input("Enter 3 grades separated by spaces ")
+        #Convert grades from string to list of integers
         list_grades = [int(x) for x in student_grades.split()]
         gradebook[student_name] = list_grades
         print(f"Student added successfully: {student_name} with grades {list_grades}")
@@ -23,6 +24,7 @@ while set == True:
                 grade_sum = sum(grades)
                 divided_grade = grade_sum / 3
                 result[key] = divided_grade
+                #The if and elif statements are to determine the letter grade by using boolean logic.
                 if divided_grade <= 60:
                     grade = "F"
                 elif divided_grade > 60 and divided_grade <= 69:
@@ -35,13 +37,16 @@ while set == True:
                     grade = "A"
                 print(f"{key}:{grades}: {result[key]:.2f}")
                 print(f"{key}'s grade: {grade}")
+                #The max function is used to determine the student with the highest average grade and the result, key=result.get is used to get the key associated with the highest value. The get method is used to retrieve the value associated with each key in the dictionary.
                 max_key = max(result, key=result.get)
                 print(f"Top student: {max_key}")
-                sortinhg = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))
-                print(f"Sorted students: {sortinhg}")
+                #Sorting the students by their average grade in descending order using sorted function and lambda function. The lambda function is used to extract the value (average grade) from each key-value pair in the dictionary for sorting. I do know what a lambda is and it's kind of similar to a def function but is more concise and is often used for short, simple functions.
+                sorting = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))
+                print(f"Sorted students: {sorting}")
         elif summary_grade == "N":
             remove_student = input("Want to remove to student? (Y/N) ").upper()
             if remove_student == "Y":
+                #The list function is used to convert the keys of the dictionary to a list so that they can be displayed to the user.
                 the_student = input(f"What student you want to remove?: {list(gradebook.keys())} ")
                 del gradebook[the_student]
                 print(gradebook)
